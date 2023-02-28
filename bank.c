6fa74a285663ad1a0a50074cc61b5863ddd8d68f
mrAmjad123 <105175479+mrAmjad123@users.noreply.github.com>
2023-02-28 10:39:02 +0530

    Add files via upload

---
+++ bank.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
+
+int nResources,
+    nProcesses;
+int *resources;
+int **allocated;
+int **maxRequired;
+int **need;
+int *safeSeq;
+int nProcessRan = 0;
+
+pthread_mutex_t lockResources;
+pthread_cond_t condition;
+
+// get safe sequence is there is one else return false
+bool getSafeSeq();
+// process function
+void* processCode(void* );
+
+int main(int argc, char** argv) {
+	srand(time(NULL));
+
+        printf("\nNumber of processes? ");
+        scanf("%d", &nProcesses);
+
+        printf("\nNumber of resources? ");
+        scanf("%d", &nResources);
+
+        resources = (int *)malloc(nResources * sizeof(*resources));
+        printf("\nCurrently Available resources (R1 R2 ...)? ");
+        for(int i=0; i<nResources; i++)
+                scanf("%d", &resources[i]);
